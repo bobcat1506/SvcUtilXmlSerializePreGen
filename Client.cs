ceafb7d659c73b8ba495060097bd34339ab16ea8
Eric Zwiebel <eric.zwiebel@gmail.com>
2019-12-18 07:28:12 -0500

    Add project files.

---
+++ Client.cs
@@ -0,0 +1,17 @@
+﻿using System.ServiceModel;
+
+namespace SvcUtilXmlSerializePreGen
+{
+    public class Client
+    {
+        public void Execute()
+        {
+            var myBinding = new BasicHttpBinding();
+            var myEndpoint = new EndpointAddress("http://localhost:2561/Service1.svc"); //Fill your service url here
+            var myChannelFactory = new ChannelFactory<IService1>(myBinding, myEndpoint);
+            IService1 client = myChannelFactory.CreateChannel();
+            string s = client.GetData(1);
+            ((ICommunicationObject)client).Close();
+        }
+    }
+}
---
+++ IService1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace SvcUtilXmlSerializePreGen
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        [XmlSerializerFormat]
+        [OperationContract(Action = "http://tempuri.org/IService1/GetData", ReplyAction = "http://tempuri.org/IService1/GetDataResponse")]
+        string GetData(int value);
+    }
+}
